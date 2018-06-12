cd715962f9f76c91b538ea777211125f47a25965
David Witter <david.witter@wisc.edu>
2018-06-12 11:44:47 -0500

    include material icon font

--- src/fonts.js
+++ src/fonts.js
@@ -3,6 +3,7 @@ $_documentContainer.setAttribute('style', 'display: none;');
 
 $_documentContainer.innerHTML = `
 <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 `
 
 document.head.appendChild($_documentContainer.content);
